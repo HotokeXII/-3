7c2772fb79ea035a92d6d7478e3ed0b52d053727
бомбяо <arskott2@gmail.com>
2024-09-29 13:49:35 +0300

    Add files via upload

---
+++ meme.py
@@ -0,0 +1,28 @@
+#ЛАБА №3
+#1.открытие и чтение файла
+#чтение всего файла
+with open('example', 'r') as file:
+    content = file.read()
+print(content)
+
+#построчное чтение
+with open('example', 'r') as file:
+    for line in file:
+        print(line)
+
+#2.запись в файл
+file = open('user_input', 'w')
+x = int(input('сколько фраз вы хотите добавить?: '))
+for i in range(1, x+1):
+    text = input('Введите текст: ')+' '
+    file.write(text)
+    print('записал')
+file.close()
+
+#3.запись в файл
+try:
+    with open('gdeblya', 'r') as file:
+        content = file.read()
+    print(content)
+except FileNotFoundError:
+    print('файл не найден')
